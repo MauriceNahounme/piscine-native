8d31ea64ec11daf4bebdee7a39610cd6b42cff2c
MauriceNahounme <mauricenahounme444@gmail.com>
2024-01-24 16:35:34 +0100

    ✅ Exercice5 : create screen folder and home page

---
+++ screen/home.screen.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Hello voici ma première page home.screen</Text>
+    </View>
+  )
+}
+
+export default Home
